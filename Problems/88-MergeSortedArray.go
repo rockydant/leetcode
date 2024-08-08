9470beebe875e08ded1a941c724f42a6414a055c
Bao Dang <dtquocbao@gmail.com>
2024-08-07 23:05:52 -0700

    feat(88): go

--- Problems/88-MergeSortedArray.go
+++ Problems/88-MergeSortedArray.go
@@ -1,2 +1,26 @@
 package main
 
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for n != 0 {
+		if m != 0 && nums1[m-1] > nums2[n-1] {
+			nums1[m+n-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[m+n-1] = nums2[n-1]
+			n--
+		}
+	}
+	for i := 0; i < len(nums1); i++ {
+		println(i)
+	}
+
+}
+
+func main() {
+	m := 3
+	n := 3
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+
+	merge(nums1, m, nums2, n)
+}
