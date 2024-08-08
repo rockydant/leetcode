c07a093514f482a88337f0a8cd838f0cd0514419
Bao Dang <dtquocbao@gmail.com>
2024-08-07 22:42:45 -0700

    format(88): C#

--- Problems/88-MergeSortedArray.cs
+++ Problems/88-MergeSortedArray.cs
@@ -1,13 +1,19 @@
-public class Solution {
-    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
         int nums1Pos = m - 1;
         int nums2Pos = n - 1;
         int totalPos = m + n - 1;
 
-        while(nums2Pos >= 0){
-            if(nums1Pos >= 0 && nums1[nums1Pos] > nums2[nums2Pos]){
+        while (nums2Pos >= 0)
+        {
+            if (nums1Pos >= 0 && nums1[nums1Pos] > nums2[nums2Pos])
+            {
                 nums1[totalPos--] = nums1[nums1Pos--];
-            }else{
+            }
+            else
+            {
                 nums1[totalPos--] = nums2[nums2Pos--];
             }
         }
